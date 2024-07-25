56e5e5216eddab2c7dd5b1d6fc34d51472bdc7a3
KeaMohlala <keamogetsemhll@gmail.com>
2024-07-25 20:11:31 +0000

    create class with gettres and setters

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,43 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new Error('Name must be a string');
+    }
+    if (typeof length !== 'number') {
+      throw new Error('Length must be a number');
+    }
+    if (!Array.isArray(students)) {
+      throw new Error('Students must be an array of strings');
+    }
+    if (!students.every((student) => typeof student === 'string')) {
+      throw new Error('All students must be strings');
+    }
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = newStudents;
+  }
+}
