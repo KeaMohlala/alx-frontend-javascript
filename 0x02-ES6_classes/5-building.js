6ffe3860100bb9c7cc685335105e2cf73c871d32
KeaMohlala <keamogetsemhll@gmail.com>
2024-07-26 17:29:09 +0000

    class inheritence

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,19 @@
+// 5-building.js
+
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
     if (typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
 
+  /* eslint-disable class-methods-use-this */
   evacuationWarningMessage() {
-    return this.evacuationWarningMessage();
   }
+  /* eslint-enable class-methods-use-this */
 }
