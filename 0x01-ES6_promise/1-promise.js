941ad6dc2a88fbdf2c878e2a5c48ba340efc89b2
KeaMohlala <keamogetsemhll@gmail.com>
2024-07-27 17:41:21 +0000

    create promise object with success response

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,10 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+      const response = { status: 200, body: 'Success' };
+      resolve(response);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
