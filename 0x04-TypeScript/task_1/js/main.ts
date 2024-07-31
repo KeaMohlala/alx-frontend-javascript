50f2e7c00382aeb43bbec7fbf2fd2c280f50719e
KeaMohlala <keamogetsemhll@gmail.com>
2024-07-31 21:02:23 +0000

    students and teachers class

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,44 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  const firstLetter = firstName.charAt(0);
+  return `${firstLetter}. ${LastName}`;
+}
+
+interface Student {
+  firstName: string;
+  lastName: string;
+}
+
+class StudentClass {
+  firstName: string;
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
